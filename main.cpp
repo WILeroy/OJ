e2c92eaaf7799ef30be037983590586a234f008d
Leroy <wileroy@163.com>
2017-11-05 13:07:17 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,133 @@
+/* 
+   问题描述 : 
+		Ackermann函数A(m,n)定义如下：
+			A(m,n) = {
+						n+1,       			m=0;
+						A(m-1, 1)  			m>0, n=0;
+						A(m-1, A(m, n-1)) 	m>0, n>0;
+					 } 
+		设计并完成A(m, n)的算法实现。 
+*/ 
+
 #include <iostream>
-#include <vector>
 
-using namespace std;
-
-int A1(int m, int n)
+/* 
+   递归方法实现 
+*/
+int A_Recursive(int m, int n)
 {
 	if (m == 0)
 	{
-		return n+1;
+		return (n+1);
 	}
 	
 	if (n == 0)
 	{
-		return A1(m-1, 1);
+		return A_Recursive(m-1, 1);
+	}
+	
+	if (m>0 && n>0)
+	{
+		int temp = A_Recursive(m, n-1);
+		return A_Recursive(m-1, temp);
+	}	
+}
+
+/*
+   动态规划方法-备忘录方法实现 
+*/ 
+int Memo[5][100000] = {0};
+int A_Memo(int m, int n)
+{
+	if (Memo[m][n] != 0)
+	{
+		return Memo[m][n];
+	}
+	
+	if (m == 0)
+	{
+		Memo[m][n] = n+1;
+		return Memo[m][n];
+	}
+	
+	if (n == 0)
+	{
+		Memo[m][n] = A_Memo(m-1, 1);
+		return Memo[m][n];
 	}
 	else
 	{
-		return A1(m-1, A1(m, n-1));
+		Memo[m][n] = A_Memo(m-1, A_Memo(m, n-1));
+		return Memo[m][n];
 	}
 }
 
-vector< vector<int> > A2_result;
+/*
+   动态规划方法-实现 
+*/ 
+int val[6];
+int ind[6];
 
-int A2(int m, int n)
+int A_Dynamic(int m, int n)
 {
-	/* Init array size to m x n */
-	A2_result.resize(m+1);
-	for (int i=0; i!=A2_result.size(); i++)
+	if (m == 0)
 	{
-		A2_result[i].resize(n+1);
+		return n+1;
+	}	
+	for (int i=0; i<=m; i++)
+	{
+		val[i] = -1;
+		ind[i] = -2;
 	}
-	
-	/* Init array 0 row */
-	for (int i=0; i!=A2_result[0].size(); i++)
+	val[0] = 1;
+	ind[0] = 0;
+	while (ind[m] < n)
 	{
-		A2_result[0][i] = i + 1;
-	}
-	
-	/* calculate other value */
-	/*
-	for (int i=1; i!=A2_result.size(); i++)
-	{
-		for (int j=0; j!=A2_result[i].size(); j++)
+		val[0]++;
+		ind[0]++;
+		for (int i=0; i<m; i++)
 		{
-			if (j == 0)
+			if (ind[i]==1 && ind[i+1]<0)
 			{
-				A2_result[i][j] = A2_result[i-1][1];
+				val[i+1] = val[0];
+				ind[i+1] = 0;
 			}
-			else
+			if (val[i+1] == ind[i])
 			{
-				A2_result[i][j] = A2_result[i-1][A2_result[i][j-1]];
+				val[i+1] = val[0];
+				ind[i+1]++;
 			}
 		}
 	}
-	*/
-	return A2_result[m][n];
+	return val[m];
 }
 
+#include <ctime>
 int main(int argc, char** argv) 
 {
-	//cout << A1(3, 12) << endl;
+	clock_t start, finish;
+	double total_time;
 	
-	cout << A2(3, 12) << endl;
+	/* 简单递归方法 */	
+	start = clock();
+	std::cout << A_Recursive(3, 11);
+	finish = clock();
+	total_time = (double)(finish-start) / CLOCKS_PER_SEC;
+	std::cout << " --- " << total_time << "s" << std::endl;
+	
+	/* 备忘录方法 */ 
+	start = clock();
+	std::cout << A_Memo(3, 11);
+	finish = clock();
+	total_time = (double)(finish-start) / CLOCKS_PER_SEC;
+	std::cout << " --- " << total_time << "s" << std::endl;
+	
+	/* 动态规划方法 */ 
+	start = clock();
+	std::cout << A_Dynamic(3, 11);
+	finish = clock();
+	total_time = (double)(finish-start) / CLOCKS_PER_SEC;
+	std::cout << " --- " << total_time << "s" << std::endl;
 	
 	return 0;
 }
